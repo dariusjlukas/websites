86f8fe1e99edda781593ccd405f4188bbae9bc31
Darius Lukas <dariusjlukas@gmail.com>
2018-09-26 11:34:30 -0400

    changed the demo and where the firebase server looks for public files

--- Inferno_test/test-app/src/App.js
+++ Inferno_test/test-app/src/App.js
@@ -2,9 +2,18 @@ import { version, Component } from 'inferno';
 import './registerServiceWorker';
 import Logo from './logo';
 import './App.css';
+import { render } from 'inferno';
+
+const message = "Hello World!";
+
+// render(
+//   <h1> message={ message } </h1>,
+//   document.getElementById("root")
+// );
 
 class App extends Component {
-  render() {
+  render()
+  {
     return (
       <div className="App">
         <header className="App-header">
@@ -14,6 +23,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>
+          {message}
+        </h2>
       </div>
     );
   }
