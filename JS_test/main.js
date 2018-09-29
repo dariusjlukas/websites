8a32e4d1e9459e0c6e011534ffa734fc94363db7
dariusjlukas <dariusjlukas@gmail.com>
2018-09-29 19:13:54 -0400

    updated a js demo project

---
+++ JS_test/main.js
@@ -0,0 +1,25 @@
+var today = new Date();
+var hourNow = today.getHours();
+var year = today.getFullYear();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if(hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+//console.log(greeting);
+gText = document.getElementById('greetingText');
+//gText.innerHTML() = greeting.toString();
+//console.log(gText);
+gText.innerHTML() = "Hello";
+//document.write('<h3>' + greeting + '</h3>');
+//document.write('<h4>' + today + '</h4>');
+
+var copyright = document.getElementById('copyright');
+copyright.innerHTML() = year.toString();
